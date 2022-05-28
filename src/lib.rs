a0fe169379db246d4aead95295575f596ec3bd4d
Shun Sakai <sorairolake@protonmail.ch>
2022-05-29 04:38:21 +0900

    Add basic features

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,33 @@
 // Copyright (C) 2022 Shun Sakai
 //
 
+//! The `sysexits` crate provides the system exit code constants as defined by
+//! [`<sysexits.h>`][sysexits-man-url].
+//!
+//! The [`SysExits`] type implements the
+//! [`Termination`](std::process::Termination) trait, so this can be returned
+//! from the `main` function.
+//!
+//! # Examples
+//!
+//! ```
+//! use sysexits::SysExits;
+//!
+//! # fn is_input_exist() -> bool {
+//! #     true
+//! # }
+//! #
+//! fn main() -> SysExits {
+//!     if !is_input_exist() {
+//!         return SysExits::NoInput;
+//!     }
+//!
+//!     SysExits::Ok
+//! }
+//! ```
+//!
+//! [sysexits-man-url]: https://man.openbsd.org/sysexits
+
 #![doc(html_root_url = "https://docs.rs/sysexits/0.1.0/")]
 // Lint levels of rustc.
 #![warn(rust_2018_idioms)]
@@ -12,11 +39,6 @@
 // Lint levels of Clippy.
 #![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod sysexits;
+
+pub use crate::sysexits::SysExits;
