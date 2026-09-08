c9ccf10d1e02f367b20a64bca875df9e405e517f
Shun Sakai <sorairolake@protonmail.ch>
2026-09-09 07:50:06 +0900

    docs: replace "method" with "function" (#300)

--- src/exit_code/result.rs
+++ src/exit_code/result.rs
@@ -16,9 +16,9 @@ pub type Result<T> = core::result::Result<T, ExitCode>;
 impl<T> From<Result<T>> for ExitCode {
     /// Converts a [`Result<T>`] into an `ExitCode`.
     ///
-    /// This method returns [`ExitCode::Ok`] if the result is [`Ok`], otherwise
-    /// returns the appropriate variant of `ExitCode` contained in the [`Err`]
-    /// variant.
+    /// This function returns [`ExitCode::Ok`] if the result is [`Ok`],
+    /// otherwise returns the appropriate variant of `ExitCode` contained in
+    /// the [`Err`] variant.
     ///
     /// # Examples
     ///
